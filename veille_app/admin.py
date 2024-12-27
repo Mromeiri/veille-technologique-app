45a0d7c0a1b077badb85b9c96520b8a33bcfe121
kamelia918 <kameliaremaki@gmail.com>
2024-12-27 16:09:33 +0100

    users + permissions

--- veille_app/admin.py
+++ veille_app/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Category, Source, Content, Task, Report
+
+# Register models here
+admin.site.register(Category)
+admin.site.register(Source)
+admin.site.register(Content)
+admin.site.register(Task)
+admin.site.register(Report)
