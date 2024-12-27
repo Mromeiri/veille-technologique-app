505ef97dab456744262fe0e082e2fc1c40519e34
kamelia918 <kameliaremaki@gmail.com>
2024-12-27 21:31:43 +0100

    added the tasks dashboard , need to fix the edit task

--- veille_app/views.py
+++ veille_app/views.py
@@ -1,14 +1,55 @@
+# from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.views import LoginView
+# from django.urls import reverse_lazy
+
+# # Create your views here.
+# @login_required
+# def index(request):
+#     # Fetch stats
+#     context = {
+#         # You can add stats to the context here
+#     }
+#     return render(request, 'index.html', context)
+
+# class Login(LoginView):
+#     template_name = 'auth-login-basic.html'
+
+#     def form_valid(self, form):
+#         # After login, redirect to the admin page
+#         return super().form_valid(form)
+
+#     def get_success_url(self):
+#         # You can customize the success URL here
+#         return reverse_lazy('admin:index')  # Redirect to Django Admin
+
+
+# from .models import Task
+
+# def kanban_board(request):
+#     tasks = Task.objects.all()  # Get all tasks
+#     context = {
+#         'tasks': tasks,
+#     }
+#     return render(request, 'kanban_board.html', context)
+
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
+from .models import Task
 
 # Create your views here.
 @login_required
 def index(request):
-    # Fetch stats
+    tasks_todo = Task.objects.filter(is_completed=False, is_in_progress=False)
+    tasks_in_progress = Task.objects.filter(is_in_progress=True)
+    tasks_completed = Task.objects.filter(is_completed=True)
+    
     context = {
-        # You can add stats to the context here
+        'tasks_todo': tasks_todo,
+        'tasks_in_progress': tasks_in_progress,
+        'tasks_completed': tasks_completed,
     }
     return render(request, 'index.html', context)
 
@@ -16,9 +57,5 @@ class Login(LoginView):
     template_name = 'auth-login-basic.html'
 
     def form_valid(self, form):
-        # After login, redirect to the admin page
+        # Add any extra logic here if needed
         return super().form_valid(form)
-
-    def get_success_url(self):
-        # You can customize the success URL here
-        return reverse_lazy('admin:index')  # Redirect to Django Admin
